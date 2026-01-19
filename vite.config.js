3a89700a44ca3c0fb873b0c30deda8083e939544
Rujit33 <rujit33@gmail.com>
2026-01-19 20:18:01 +0545

    fixed git hub pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/github-to-cv/",
   define: {
     // Make env variables available
     "process.env": {},
   },
   build: {
+    outDir: "docs",
     rollupOptions: {
       // Exclude latex.js from bundle since we use HTML fallback
       external: ["latex.js"],
